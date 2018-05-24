b68b8a96beeffe0340963496b4325b94aa078573
Emmanuel Addo Narh <enarh@visa.com>
2018-05-24 09:09:56 -0700

    feat: removed demo pages

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { Provider, subscribe } from 'react-contextual';
 
 import './index.css';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 // Components
 import header from './components/header';
 import sideNavigation from './components/side-navigation';
 
 // Pages
-// import index from './pages/index';
-// import login from './pages/login';
+import index from './pages/index';
+import login from './pages/login';
 import createAccount from './pages/create-account';
-// import settings from './pages/settings';
+import settings from './pages/settings';
 
 import { initialState, actions } from './state';
 
 const Header = subscribe()(header);
 const SideNavigation = subscribe()(sideNavigation);
 
-// const Index = subscribe()(index);
-// const Login = subscribe()(login);
-// const Settings = subscribe()(settings);
+const Index = subscribe()(index);
+const Login = subscribe()(login);
+const Settings = subscribe()(settings);
 const CreateAccount = subscribe()(createAccount);
 
-// const ProtectedRoute = subscribe()(({
-//   component: Component,
-//   isAuthenticated,
-//   ...rest
-// }) => (
-//   <Route
-//     {...rest}
-//     render={props =>
-//       isAuthenticated
-//         ? (<Component {...props} />)
-//         : (<Redirect to={{ pathname: '/login', state: { from: props.location } }} />)}
-//   />
-// ));
+const ProtectedRoute = subscribe()(({
+  component: Component,
+  isAuthenticated,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated
+        ? (<Component {...props} />)
+        : (<Redirect to={{ pathname: '/login', state: { from: props.location } }} />)}
+  />
+));
 
 ReactDOM.render(
   <Provider initialState={initialState} actions={actions} >
@@ -47,7 +47,10 @@ ReactDOM.render(
         <Header />
         <SideNavigation />
         <main className='main-content'>
-          <Route exact path='/' component={CreateAccount} />
+          <ProtectedRoute exact path='/' component={Index} />
+          <Route path='/login' component={Login} />
+          <Route path='/create-account' component={CreateAccount} />
+          <Route path='/settings' component={Settings} />
         </main>
       </React.Fragment>
     </BrowserRouter>
@@ -55,22 +58,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// ReactDOM.render(
-//   <Provider initialState={initialState} actions={actions} >
-//     <BrowserRouter>
-//       <React.Fragment>
-//         <Header />
-//         <SideNavigation />
-//         <main className='main-content'>
-//           <ProtectedRoute exact path='/' component={Index} />
-//           <Route path='/login' component={Login} />
-//           <Route path='/create-account' component={CreateAccount} />
-//           <Route path='/settings' component={Settings} />
-//         </main>
-//       </React.Fragment>
-//     </BrowserRouter>
-//   </Provider>,
-//   document.getElementById('root')
-// );
-
-// registerServiceWorker();
+registerServiceWorker();
